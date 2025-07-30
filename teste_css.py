7e2c51551126f2b087b65a63a763f9e16507c0ac
leosturmer <leosturmer@email>
2025-07-30 12:03:34 -0300

    Fazendo uma aplicação básica com Textual e CSS

--- teste_css.py
+++ teste_css.py
@@ -1,30 +1,63 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Static, Button, Input, Label, Header, Footer, TextArea
+from textual.widgets import Static, Button, Input, Header, Footer, TextArea
+from textual.containers import HorizontalGroup
 
 class UmaTela(App):
+
+    CSS_PATH = 'teste_css.css'
     
-    TITLE = 'Brincando com CSS'
-    SUB_TITLE = 'Testando o que fazer com o CSS por aqui'
+    TITLE = 'Digite o seu texto'
+    SUB_TITLE = 'E veja ele na área de visualização'
 
     def compose(self) -> ComposeResult:
-        yield Header(id='header')
+        yield Header(show_clock=True)
 
         yield Static('''
 
-Este é um programa para brincar com o uso do CSS!
+Escreva um texto e clique em salvar.
+O texto irá aparecer na área de visualização abaixo, uma linha por vez.
+                     
+''', id='static1')
 
-''')
+        # yield Label (id='label1')
 
-        yield Label ()
+        with HorizontalGroup():
+            yield Input(placeholder='Insira aqui o texto...', id='input1')
 
-        yield Input(placeholder='Insira aqui o texto...')
+        with HorizontalGroup(id="container"):
+        
+            yield Button('Salvar texto', id='bt_salvar', classes='btn')
+            yield Button('Limpar', id='bt_limpar', classes='btn')
+            yield Button('Deletar texto', id='bt_deletar', classes='btn')           
 
-        yield Button('Salvar texto', id='bt_cadastrar', classes='btn')
-        yield Button('Limpar texto', id='bt_limpar', classes='btn')
 
-        yield TextArea(disabled=True)
+        yield TextArea(read_only=True, show_cursor=False, id='textarea')
 
         yield Footer()
+
+    def on_button_pressed(self, event: Button.Pressed):
+        if event.button.id == 'bt_limpar':
+            for text in self.query('Input'):
+                text.value = ''
+            self.query_one('#input1').focus()
+
+        if event.button.id == 'bt_deletar':
+            self.query_one('#textarea', TextArea).text = ''
+            self.query_one('#input1').focus()
+            self.query_one('#input1', Input).value = ''
+
+
+        if event.button.id == 'bt_salvar':
+            if self.query_one('#input1', Input).value == '':
+                self.query_one('#input1').focus()
+                self.query_one('#input1', Input).value = ''
+            else:
+                texto_input = self.query_one('#input1', Input).value
+
+                self.query_one('#textarea', TextArea).text += f'{texto_input} \n'
+                self.query_one('#input1').focus()
+                self.query_one('#input1', Input).value = ''
+
 
 
 if __name__ == "__main__":
